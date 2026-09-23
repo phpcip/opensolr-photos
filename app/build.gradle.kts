4c95a2d2cc3f179c5fa76395139174c4298f2d17
Ciprian Dimofte <cip@opensolr.com>
2026-09-23 03:23:40 +0300

    Release 3.10.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.opensolr.photos"
         minSdk = 26
         targetSdk = 36
-        versionCode = 96
-        versionName = "3.10.0"
+        versionCode = 97
+        versionName = "3.10.1"
     }
 
     flavorDimensions += "store"
